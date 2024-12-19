e5b3c0fd284b8cda119d2e25b06dd7424230e333
Yujin <evfavorito@gbox.ncf.edu.ph>
2024-12-19 17:09:08 +0800

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -52,15 +52,17 @@ const updateUser = async (req, res) => {
 
 const deleteUser = async (req, res) => {
   const { id } = req.params;
+
   try {
     const [result] = await pool.query('DELETE FROM users WHERE user_id = ?', [id]);
+
     if (result.affectedRows === 0) {
       return res.status(404).json({ error: 'User not found' });
     }
-    res.json({ message: 'User deleted successfully' });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+
+    res.json({ message: 'Na Delete mo na ang iyong Sarili Congrats!' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 };
-
 module.exports = { getAllUsers, getUserById, createUser, updateUser, deleteUser };
